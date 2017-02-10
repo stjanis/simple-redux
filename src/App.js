42ec6ae60cb2a8981d204c767276b4cf8ebeee17
stjanis <janis.stipnieks@novasol.com>
2017-02-10 16:12:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => <div>test</div>;
+
+export default App;
+
+// https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3#.fsrmt1ouj
