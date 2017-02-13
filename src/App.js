9dd68c47e06bb011f069cdef1f968a496b754d3e
stjanis <janis.stipnieks@novasol.com>
2017-02-13 10:48:52 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -4,5 +4,3 @@ import ItemList from './components/ItemList';
 const App = () => <ItemList />;
 
 export default App;
-
-// https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3#.fsrmt1ouj
