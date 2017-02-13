938147008ce5e5d1b165b185ec0727d95ed98cd3
stjanis <janis.stipnieks@novasol.com>
2017-02-13 15:36:59 +0100

    configureStore import bug fix

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/items';
 
 class ItemList extends Component {
   constructor() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from '.store/configureStore';
+import configureStore from './store/configureStore';
 import ItemList from './components/ItemList';
+
+const store = configureStore();
 
 render(
   <Provider store={store}>
