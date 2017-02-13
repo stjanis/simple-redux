c974e84c241e23d17d14ebf85b6a0f15df5a5eb1
stjanis <janis.stipnieks@novasol.com>
2017-02-13 11:23:22 +0100

    fetch data from REST endpoint

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,47 +1,54 @@
 import React, { Component } from 'react';
 
 class ItemList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            items: [
-                {
-                    id: 1,
-                    label: 'List item 1'
-                },
-                {
-                    id: 2,
-                    label: 'List item 2'
-                },
-                {
-                    id: 3,
-                    label: 'List item 3'
-                },
-                {
-                    id: 4,
-                    label: 'List item 4'
-                }
-            ],
-            hasErrored: false,
-            isLoading: false
-        };
-    }
-    render() {
-        if (this.state.hasErrored) {
-            return <p>Sorry! There was an error loading the items</p>;
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      hasErrored: false,
+      isLoading: false
+    };
+  }
+
+  fetchData(url) {
+    this.setState({ isLoading: true });
+
+    fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw Error(response.statusText);
         }
-        if (this.state.isLoading) {
-            return <p>Loading…</p>;
-        }
-        return (
-            <ul>
-                {this.state.items.map((item) => (
-                    <li key={item.id}>
-                        {item.label}
-                    </li>
-                ))}
-            </ul>
-        );
+
+        this.setState({ isLoading: false });
+
+        return response;
+      })
+        .then(response => response.json())
+        .then(items => this.setState({ items }))
+        .catch(() => this.setState({ hasErrored: true }));
+  }
+
+  componentDidMount() {
+    this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return <p>Sorry! There was an error loading the items</p>;
     }
+    if (this.state.isLoading) {
+      return <p>Loading…</p>;
+    }
+    return (
+      <ul>
+        {this.state.items.map((item) => (
+          <li key={item.id}>
+            {item.label}
+          </li>
+         ))}
+      </ul>
+    );
+  }
 }
+
 export default ItemList;
